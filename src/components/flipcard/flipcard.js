e9870bc2b8cf2c47f1d855ae93aa557982d16bc6
YI-CHEN HSU <126523675+Janjanhsu@users.noreply.github.com>
2023-11-21 17:02:28 -0500

    Add files via upload
    *Fixed the counting of the correct number of answers, and changed API to ask animal questions (CS questions are too difficult to test).

--- src/components/flipcard/flipcard.js
+++ src/components/flipcard/flipcard.js
@@ -7,36 +7,32 @@ function Flipcard() {
     const [numOfCorrectQuestions, setNumOfCorrectQuestions] = useState(0);
 
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean")
+        fetch("https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=boolean")
             .then(response => response.json())
             .then(data => setQuestion(data.results))
     }, []);
 
     const nextQuestion = (e) => {
-        if(correct_answer == e.value){
-            setNumOfCorrectQuestions( e => e++);
+        if (correct_answer === e.target.value) {
+            setNumOfCorrectQuestions(numOfCorrectQuestions + 1);
         }
         console.log(numOfCorrectQuestions);
-        setCurrentQuestionIndex(prevIndex => (prevIndex < 9)? prevIndex + 1: prevIndex = 0);
+        setCurrentQuestionIndex(prevIndex => (prevIndex < 9) ? (prevIndex + 1) : setQuestion([]));
     };
 
-    const {question, correct_answer} = questions[currentQuestionIndex] || {};
-    
+    const { question, correct_answer } = questions[currentQuestionIndex] || {};
+
     return (
         <>
             <div className="card-container">
                 {questions.length > 0 ? (
                     <div className="flip-card">
                         <div className="flip-card-inner">
-                            <div className="flip-card-front"> 
+                            <div className="flip-card-front">
                                 <h3>Question {currentQuestionIndex + 1}</h3>
-                                <p dangerouslySetInnerHTML={{__html:question}}/>
-                                <button value = "True" onClick={nextQuestion}>True</button> 
-                                <button value = "False" onClick={nextQuestion}>False</button>                                 
-                            </div>
-                            <div className="flip-card-back">asdfasdfas
-                                <h3>Answer</h3>
-                                <p>{question.correct_answer}</p>
+                                <p dangerouslySetInnerHTML={{ __html: question }} />
+                                <button value="True" onClick={nextQuestion}>True</button>
+                                <button value="False" onClick={nextQuestion}>False</button>
                             </div>
                         </div>
                     </div>
