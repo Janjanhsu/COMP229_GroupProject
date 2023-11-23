a0ccc8fef7174cac7fddcca0d0ea406eb1700d0f
Atcareva <114267115+Atcareva@users.noreply.github.com>
2023-11-23 15:07:47 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './assets/images/logo.jpg';
 import './App.css';
-
-import Flipcard from './components/flipcard/flipcard.js';
-
+import NavBar from './components/NavBar';
+import SignIn from './components/SignIn';
+import LogIn from './components/LogIn';
+import FlashcardQuiz from './components/FlashcardQuiz';
+import Ranking from './components/Ranking';
 
 function App() {
   return (
-    <>
+    <Router>
       <header className="app-header">
-        <img src={logo} className="logo" alt="logo" />
+        <Link to="/">
+          <img src={logo} className="logo" alt="logo" />
+        </Link>
+        <NavBar />
       </header>
 
-      <div className="App">
-        <h1>Flip Card Quiz</h1>
-        <Flipcard />
-      </div>
-
-
-    </>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/flashcard-quiz" element={<FlashcardQuiz />} />
+        <Route path="/ranking" element={<Ranking />} />
+        <Route path="/" element={
+          <div className="App">
+            <h1>Welcome to the Home Page</h1>
+            <Link to="/flashcard-quiz" className="big-button">Go to FlashCard Quiz</Link>
+          </div>
+        } />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
