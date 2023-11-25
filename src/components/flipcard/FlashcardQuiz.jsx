efdf8ca043bf60f5441691f2967b14a86f3db018
tbbfff <youremail@example.comtbrunobf@gmail.com>
2023-11-25 10:02:13 -0500

    alternative organizaiton

---
+++ src/components/flipcard/FlashcardQuiz.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Flipcard from './flipcard'; 
+
+const FlashcardQuiz = () => {
+  return (
+    <div>
+      <h1>Flashcard Quiz</h1>
+      <Flipcard /> 
+    </div>
+  );
+};
+
+export default FlashcardQuiz;
