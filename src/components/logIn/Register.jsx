fb76d8301842f370680f562072699847059a64f2
Ricardoonz <98755098+Ricardoonz@users.noreply.github.com>
2023-12-02 18:29:32 -0500

    improved UI for Register Page

--- src/components/logIn/Register.jsx
+++ src/components/logIn/Register.jsx
@@ -1,18 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../../styles/Register.css'; 
 
-const SignUp = () => {
+const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Registration failed');
+      }
+
+      // Registration successful, perform any necessary actions (e.g., redirect to login)
+    } catch (err) {
+      setError('Registration failed');
+    }
+  };
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-        <button type="submit">Sign In</button>
+    <div className="register-container">
+      <h2>Register</h2>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleRegister}>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
       </form>
     </div>
   );
 };
 
-export default SignUp;
+export default Register;
