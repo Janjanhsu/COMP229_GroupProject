1d6017b68f22d574f104b9259eb4562304dafa9e
Ricardoonz <98755098+Ricardoonz@users.noreply.github.com>
2023-12-07 15:24:10 -0500

    added database and improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,39 @@ import LogIn from './components/logIn/LogIn';
 import FlashcardQuiz from './components/flipcard/FlashcardQuiz';
 import Ranking from './components/ranking/Ranking';
 
+//import { dbConnect } from './database/db';
+
 function App() {
   return (
     <Router>
-      <header className="app-header">
-        <Link to="/">
-          <img src={logo} className="logo" alt="logo" />
-        </Link>
-        <NavBar />
-      </header>
+      <div className="app">
+        <header className="app-header">
+          <Link to="/">
+            <img src={logo} className="logo" alt="logo" />
+          </Link>
+          <NavBar />
+        </header>
 
-      <Routes>
-        <Route path="/logIn/SignUp" element={<SignUp />} />
-        <Route path="/logIn/LogIn" element={<LogIn />} />
-        <Route path="/flashcard-quiz" element={<FlashcardQuiz />} />
-        <Route path="/ranking/ranking" element={<Ranking />} />
-        <Route path="/" element={
-          <div className="App">
-            <h1>Welcome to the Home Page</h1>
-            <Link to="/flashcard-quiz" className="big-button">Go to FlashCard Quiz</Link>
-          </div>
-        } />
-      </Routes>
+        <main className="app-content">
+          <Routes>
+            <Route path="/logIn/SignUp" element={<SignUp />} />
+            <Route path="/logIn/LogIn" element={<LogIn />} />
+            <Route path="/flashcard-quiz" element={<FlashcardQuiz />} />
+            <Route path="/ranking/ranking" element={<Ranking />} />
+            <Route
+              path="/"
+              element={
+                <div className="home">
+                  <h1>Welcome to the Home Page</h1>
+                  <Link to="/flashcard-quiz" className="big-button">
+                    Go to FlashCard Quiz
+                  </Link>
+                </div>
+              }
+            />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
