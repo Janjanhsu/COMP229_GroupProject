180694b4f0bcae0e85ba0827c9cf12f9a9efd278
Yza <yasc11003@gmail.com>
2023-12-08 21:30:47 -0500

    merge fullstack3.0

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="./logIn/SignUp">Sign Up</Link></li>
-        <li><Link to="./logIn/LogIn">Log In</Link></li>
-        <li><Link to="./flashcard-quiz">Flashcard Quiz</Link></li> 
+        <li className="left"><Link to="./flashcard-quiz">Flashcard Quiz</Link></li> 
         <li><Link to="./ranking/Ranking">Ranking</Link></li>
+        <li className="right"><Link to="/logIn/SignUp">Sign Up</Link></li>
+        <li><Link to="/logIn/LogIn">Log In</Link></li>
       </ul>
     </nav>
   );
