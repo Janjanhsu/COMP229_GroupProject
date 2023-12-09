739268b275ec044a00a13bdb260af535fc1f3993
YI-CHEN HSU <126523675+Janjanhsu@users.noreply.github.com>
2023-12-09 10:14:47 -0500

    reverted manually

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import config from './config/config.js'
 import app from './server/express.js'
-import mongoose from 'mongoose';
-
-mongoose.Promise = global.Promise
+import mongoose from 'mongoose'
+mongoose.Promise = globalThis.Promise
 mongoose.connect(config.mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
